26656bd5d3fa3390c549c7c59278b28cb26fec50
Luke Todd <luketodd@zoho.com>
2022-01-09 14:35:06 +0000

    Split out the FE and backend as there were clashes with eslint

--- README.md
+++ README.md
@@ -1,7 +1,9 @@
 # Solidity Learning
 
 
-Currently running through the Buidl Guidl scaffold-eth tuts.
+Currently working on a treasure chat gifting full stack application.
+
+This will store an amount of a token to be received by someone with a specific ENS/address when they enter the site.
 
 ## Faucet
 - https://faucets.chain.link/rinkeby
---
+++ treasurechest/app/src/App.tsx
@@ -0,0 +1,54 @@
+import './App.css'
+import { useState } from 'react'
+import { ethers } from 'ethers'
+import Greeter from './artifacts/contracts/Greeter.sol/Greeter.json'
+
+const greeterAddress = '0x5FbDB2315678afecb367f032d93F642f64180aa3'
+
+const App = () => {
+  const [greeting, setGreetingValue] = useState('')
+
+  const requestAccount = async () => {
+    await (window as any).ethereum.request({ method: 'eth_requestAccounts' })
+  }
+
+  const fetchGreeting = async () => {
+    if (typeof (window as any).ethereum !== 'undefined') {
+      const provider = new ethers.providers.Web3Provider((window as any).ethereum)
+      const contract = new ethers.Contract(greeterAddress, Greeter.abi, provider)
+
+      try {
+        const data = await contract.greet()
+        console.log(`data: ${data}`)
+      } catch (err) {
+        console.log('Error: ', err)
+      }
+    }
+  }
+
+  const setGreeting = async () => {
+    if (!greeting) return
+
+    if (typeof (window as any).ethereum !== 'undefined') {
+      await requestAccount()
+      const provider = new ethers.providers.Web3Provider((window as any).ethereum)
+      const signer = provider.getSigner()
+      const contract = new ethers.Contract(greeterAddress, Greeter.abi, signer)
+      const transaction = await contract.setGreeting(greeting)
+      await transaction.wait()
+      fetchGreeting()
+    }
+  }
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        <button onClick={fetchGreeting}>Fetch Greeting</button>
+        <button onClick={setGreeting}>Set Greeting</button>
+        <input onChange={e => setGreetingValue(e.target.value)} placeholder="Set greeting" type="text" />
+      </header>
+    </div>
+  )
+}
+
+export default App
